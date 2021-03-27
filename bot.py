5fcb1361e58ee60e220d46fa484940278d3c2e02
monosans <hsyqixco@protonmail.com>
2021-03-28 00:54:58 +0300

    Версия 0.9. Красивый вывод, настройка цепей для невидимок, проверка на правильность конфига, небольшая оптимизация, улучшение инструкции

--- bot.py
+++ bot.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from json import load, loads
+import sys
+from json import load
 from random import randint, random
 from threading import Thread
-from time import sleep
+from time import sleep, strftime
 
-import requests
+from requests import get, post
 
 
 def buy_slave():
-    """
-    Покупает рабов, даёт работу. Надевает оковы, если включено в config.json.
-    """
+    """Покупает рабов и оковы, если включено в config.json."""
     while True:
         try:
             if config["invisible_slaves"] == 1:
@@ -19,9 +18,9 @@ def buy_slave():
                 rand_slave = randint(-999999999, -1)
             else:
                 # Случайный ID пользователя в промежутке
-                rand_slave = randint(1, 646412830)
+                rand_slave = randint(1, 646735737)
             # Покупка раба
-            buySlave = requests.post(
+            buySlave = post(
                 "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/buySlave",
                 headers={
                     "Content-Type": "application/json",
@@ -31,17 +30,20 @@ def buy_slave():
                 json={"slave_id": rand_slave},
             )
 
-            # Вывод информации о профиле
-            profile = loads(buySlave.text)
+            # Получение информации о профиле
+            profile = buySlave.json()
+
             print(
-                f"""Баланс: {profile['balance']}
+                f"""\n===[{strftime("%d.%m.%Y %H:%M:%S")}]===
+Баланс: {profile['balance']}
 Рабов: {profile['slaves_count']}
-Доход в минуту: {profile['slaves_profit_per_min']}"""
+Доход в минуту: {profile['slaves_profit_per_min']}
+===========================\n"""
             )
 
-            # Покупает оковы
-            if config["buy_fetters"] == 1:
-                fetter_request = requests.post(
+            # Покупает оковы только что купленному рабу
+            if config["buy_fetters"] == 1 and config["invisible_fetters"] == 1:
+                fetter_request = post(
                     "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/buyFetter",
                     headers={
                         "Content-Type": "application/json",
@@ -52,12 +54,11 @@ def buy_slave():
                         "slave_id": rand_slave,
                     },
                 )
-                fetter_text = fetter_request.text
-                print(f"Купил оковы vk.com/id{loads(fetter_text)['id']}")
+                fetter_text = fetter_request.json()
+                print(f"Купил оковы vk.com/id{fetter_text['id']}")
             sleep(delay + random())
         except Exception as e:
-            if not "line" in str(e):
-                print(e)
+            print(e.args)
             sleep(delay + random())
 
 
@@ -66,27 +67,21 @@ def buy_fetter():
     while True:
         try:
             # Получение полной информации об аккаунте
-            start = loads(
-                requests.get(
-                    "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/start",
-                    headers={
-                        "Content-Type": "application/json",
-                        "authorization": auth,
-                        "User-agent": "Mozilla/5.0",
-                    },
-                ).text,
-            )
+            start = get(
+                "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/start",
+                headers={
+                    "Content-Type": "application/json",
+                    "authorization": auth,
+                    "User-agent": "Mozilla/5.0",
+                },
+            ).json()
 
-            if config["buy_slaves"] == 1:
-                # Удаление из списка первого раба,
-                # чтобы не происходило коллизии с методом buy_slave
-                del start["slaves"][0]
             # Перебор списка рабов
             for slave in start["slaves"]:
                 # Проверка на наличие оков
-                if slave["fetter_to"] == 0 and slave["id"] >= 1:
+                if int(slave["fetter_to"]) == 0 and int(slave["id"]) >= 1:
                     # Покупка оков
-                    requests.post(
+                    post(
                         "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/buyFetter",
                         headers={
                             "Content-Type": "application/json",
@@ -94,14 +89,34 @@ def buy_fetter():
                             "User-agent": "Mozilla/5.0",
                         },
                         json={
-                            "slave_id": slave["id"],
+                            "slave_id": int(slave["id"]),
                         },
                     )
                     print(f"Купил оковы vk.com/id{slave['id']}")
                     sleep(delay + random())
+
+                # Покупка оков невидимкам, если включено в конфиге
+                elif (
+                    int(slave["fetter_to"]) == 0
+                    and config["invisible_fetters"] == 1
+                ):
+                    # Покупка оков
+                    post(
+                        "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/buyFetter",
+                        headers={
+                            "Content-Type": "application/json",
+                            "authorization": auth,
+                            "User-agent": "Mozilla/5.0",
+                        },
+                        json={
+                            "slave_id": int(slave["id"]),
+                        },
+                    )
+                    print(f"Купил оковы vk.com/id{slave['id']}")
+                    sleep(delay + random())
+
         except Exception as e:
-            if not "line" in str(e):
-            	print(e)
+            print(e.args)
             sleep(delay + random())
 
 
@@ -109,23 +124,21 @@ def job_slave():
     """Даёт безработным работу."""
     while True:
         try:
-            start = loads(
-                requests.get(
-                    "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/start",
-                    headers={
-                        "Content-Type": "application/json",
-                        "authorization": auth,
-                        "User-agent": "Mozilla/5.0",
-                    },
-                ).text,
-            )
+            start = get(
+                "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/start",
+                headers={
+                    "Content-Type": "application/json",
+                    "authorization": auth,
+                    "User-agent": "Mozilla/5.0",
+                },
+            ).json()
 
             # Перебор списка рабов
             for slave in start["slaves"]:
                 # Проверка на наличие у раба работы
                 if not slave["job"]["name"]:
                     # Даёт рабу работу
-                    requests.post(
+                    post(
                         "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/jobSlave",
                         headers={
                             "Content-Type": "application/json",
@@ -133,27 +146,35 @@ def job_slave():
                             "User-agent": "Mozilla/5.0",
                         },
                         json={
-                            "slave_id": slave["id"],
+                            "slave_id": int(slave["id"]),
                             "name": job,
                         },
                     )
+
                     print(f"Дал работу vk.com/id{slave['id']}")
                     sleep(delay + random())
         except Exception as e:
-            if not "line" in str(e):
-            	print(e)
+            print(e.args)
             sleep(delay + random())
 
 
 if __name__ == "__main__":
+    print("Версия 0.9")
+
+    # Конфиг
     with open("config.json") as f:
-        config = load(f)
-    auth = config["authorization"]
-    delay = config["delay"]
-    job = config["job"]
-    print("Бот запущен")
-    if config["buy_slaves"] == 1:
+        try:
+            config = load(f)
+        except Exception as e:
+            print("Неверный конфиг")
+            sys.exit()
+    auth = str(config["authorization"])
+    delay = int(config["delay"])
+    job = str(config["job"])
+
+    # Запуск
+    if int(config["buy_slaves"]) == 1:
         Thread(target=buy_slave).start()
-    if config["buy_fetters"] == 1:
+    if int(config["buy_fetters"]) == 1:
         Thread(target=buy_fetter).start()
     Thread(target=job_slave).start()
