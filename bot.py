0267301a6a7cca0619afc9b63d6e160339e4997b
monosans <hsyqixco@protonmail.com>
2021-03-28 21:33:29 +0300

    Revert "Версия 2.1. Исправил баг, из-за которого не покупались оковы при upgrade_slaves:0. Улучшил структуру кода."
    This reverts commit 2a64bfc161f7cdcb189e51d6ab987f63096c9dd6.

--- bot.py
+++ bot.py
@@ -11,44 +11,68 @@ from requests import get, post
 
 def buy_slave(id):
     """Покупает раба."""
-    post(
-        "https:/pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/buySlave",
-        headers=headers,
+    return post(
+        "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/buySlave",
+        headers={
+            "Content-Type": "application/json",
+            "authorization": auth,
+            "User-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.105 Safari/537.36",
+            "origin": "https://prod-app7794757-c1ffb3285f12.pages-ac.vk-apps.com",
+        },
         json={"slave_id": id},
-    )
+    ).json()
 
 
 def buy_fetter(id):
     """Покупает оковы."""
     post(
         "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/buyFetter",
-        headers=headers,
-        json={"slave_id": id},
+        headers={
+            "Content-Type": "application/json",
+            "authorization": auth,
+            "User-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.105 Safari/537.36",
+            "origin": "https://prod-app7794757-c1ffb3285f12.pages-ac.vk-apps.com",
+        },
+        json={
+            "slave_id": id,
+        },
     )
 
 
 def get_start():
-    """Получает полную информацию о своём профиле."""
     return get(
         "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/start",
-        headers=headers,
+        headers={
+            "Content-Type": "application/json",
+            "authorization": auth,
+            "User-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.105 Safari/537.36",
+            "origin": "https://prod-app7794757-c1ffb3285f12.pages-ac.vk-apps.com",
+        },
     ).json()
 
 
 def sell_slave(id):
-    """Продаёт раба."""
     post(
         "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/saleSlave",
-        headers=headers,
+        headers={
+            "Content-Type": "application/json",
+            "authorization": auth,
+            "User-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.105 Safari/537.36",
+            "origin": "https://prod-app7794757-c1ffb3285f12.pages-ac.vk-apps.com",
+        },
         json={"slave_id": id},
     )
 
 
 def job_slave(id):
-    """Даёт работу."""
     post(
         "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/jobSlave",
-        headers=headers,
+        headers={
+            "Content-Type": "application/json",
+            "authorization": auth,
+            "User-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.105 Safari/537.36",
+            "origin": "https://prod-app7794757-c1ffb3285f12.pages-ac.vk-apps.com",
+        },
         json={
             "slave_id": id,
             "name": choice(job),
@@ -57,10 +81,14 @@ def job_slave(id):
 
 
 def get_user(id):
-    """Получает информацие о пользователе."""
     return get(
         "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/user",
-        headers=headers,
+        headers={
+            "Content-Type": "application/json",
+            "authorization": auth,
+            "User-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.105 Safari/537.36",
+            "origin": "https://prod-app7794757-c1ffb3285f12.pages-ac.vk-apps.com",
+        },
         params={"id": id},
     ).json()
 
@@ -81,24 +109,24 @@ def buy_slaves():
                 rand_slave = randint(1, 646735737)
                 rand_slave_info = get_user(rand_slave)
 
-            # Покупка раба
-            buy_slave(rand_slave)
-
-            # Получение информации о себе
-            me = get_user(my_id)
+            # Покупка раба и получение информации о своём профиле
+            profile = buy_slave(rand_slave)
 
             print(
-                f"""\n==[{strftime("%d.%m.%Y %H:%M:%S")}]==
+                f"""\n===[{strftime("%d.%m.%Y %H:%M:%S")}]===
 Купил vk.com/id{rand_slave} за {rand_slave_info["price"]}
-Баланс: {"{:,}".format(me['balance'])}
-Рабов: {"{:,}".format(me['slaves_count'])}
-Доход в минуту: {"{:,}".format(me['slaves_profit_per_min'])}
-Место в рейтинге: {"{:,}".format(me['rating_position'])}\n""",
+Баланс: {"{:,}".format(profile['balance'])}
+Рабов: {"{:,}".format(profile['slaves_count'])}
+Доход в минуту: {"{:,}".format(profile['slaves_profit_per_min'])}
+Место в рейтинге: {"{:,}".format(profile['rating_position'])}
+===========================\n""",
             )
             if upgrade_slaves == 1:
+                # Получение полной информации об аккаунте
+                me = get_user(my_id)
+
                 # Перебор списка рабов
                 if "balance" in me.keys():
-                    # Проверка на то, хватит ли баланса для прокачки
                     if int(me["balance"]) >= 39214:
                         while int(get_user(rand_slave)["price"]) <= 26151:
                             sell_slave(rand_slave)
@@ -107,10 +135,10 @@ def buy_slaves():
                             print("Улучшил раба")
                             sleep(delay + random())
 
-            # Покупает оковы только что купленному рабу
-            if buy_fetters == 1:
-                buy_fetter(rand_slave)
-                print(f"Купил оковы vk.com/id{rand_slave}")
+                # Покупает оковы только что купленному рабу
+                if buy_fetters == 1:
+                    buy_fetter(rand_slave)
+                    print(f"Купил оковы vk.com/id{rand_slave}")
 
             sleep(delay + random())
         except Exception as e:
@@ -159,8 +187,8 @@ def job_slaves():
 if __name__ == "__main__":
     print(
         """vk.com/free_slaves_bot
-github.com/monosans/vk-slaves-bot
-Версия 2.1""",
+github.com/monosans/free-slaves-bot
+Версия 2.0""",
     )
 
     # Конфиг
@@ -174,22 +202,11 @@ github.com/monosans/vk-slaves-bot
     conf_buy_fetters = int(config["buy_fetters"])
     conf_buy_slaves = int(config["buy_slaves"])
     delay = int(config["delay"])
-    try:
-        job = list(config["job"])
-    except:
-        job = str(config["job"])
+    job = list(config["job"])
     max_price = int(config["max_price"])
     min_price = int(config["min_price"])
     my_id = int(config["my_id"])
     upgrade_slaves = int(config["upgrade_slaves"])
-
-    # Информация, нужная для обращения к серверу
-    headers = {
-        "Content-Type": "application/json",
-        "authorization": auth,
-        "User-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.105 Safari/537.36",
-        "origin": "https://prod-app7794757-c1ffb3285f12.pages-ac.vk-apps.com",
-    }
 
     # Запуск
     if conf_buy_slaves == 1:
