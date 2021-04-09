fc02ddea96bb85574c75d7782c2b4757e9fcea08
monosans <hsyqixco@protonmail.com>
2021-04-09 22:58:31 +0300

    Чистка

--- bot.py
+++ bot.py
@@ -1,24 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import sys
-from json import load
 from random import choice, randint, uniform
 from threading import Thread
 from time import sleep, strftime
 
 from requests import get, post
 
+from config import (
+    authorization,
+    buy_fetters,
+    buy_from_ids,
+    buy_slaves_mode,
+    jobs,
+    max_delay,
+    max_fetter_price,
+    max_price,
+    min_delay,
+    min_price,
+    my_id,
+    upgrade_slaves,
+)
+
 
 def buy_slave(id):
     """Покупает раба."""
     post(
         "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/buySlave",
-        headers={
-            "Content-Type": "application/json",
-            "authorization": auth,
-            "User-agent": user_agent,
-            "origin": origin,
-        },
+        headers=headers,
         json={"slave_id": id},
     )
 
@@ -27,12 +35,7 @@ def get_buy_slave(id):
     """Покупает раба."""
     return post(
         "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/buySlave",
-        headers={
-            "Content-Type": "application/json",
-            "authorization": auth,
-            "User-agent": user_agent,
-            "origin": origin,
-        },
+        headers=headers,
         json={"slave_id": id},
     ).json()
 
@@ -41,12 +44,7 @@ def buy_fetter(id):
     """Покупает оковы."""
     post(
         "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/buyFetter",
-        headers={
-            "Content-Type": "application/json",
-            "authorization": auth,
-            "User-agent": user_agent,
-            "origin": origin,
-        },
+        headers=headers,
         json={"slave_id": id},
     )
 
@@ -55,12 +53,7 @@ def sell_slave(id):
     """Продаёт раба."""
     post(
         "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/saleSlave",
-        headers={
-            "Content-Type": "application/json",
-            "authorization": auth,
-            "User-agent": user_agent,
-            "origin": origin,
-        },
+        headers=headers,
         json={"slave_id": id},
     )
 
@@ -69,15 +62,10 @@ def job_slave(id):
     """Даёт работу."""
     post(
         "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/jobSlave",
-        headers={
-            "Content-Type": "application/json",
-            "authorization": auth,
-            "User-agent": user_agent,
-            "origin": origin,
-        },
+        headers=headers,
         json={
             "slave_id": id,
-            "name": choice(job),
+            "name": choice(jobs),
         },
     )
 
@@ -86,12 +74,7 @@ def get_user(id):
     """Получает информацие о пользователе."""
     return get(
         f"https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/user?id={id}",
-        headers={
-            "Content-Type": "application/json",
-            "authorization": auth,
-            "User-agent": user_agent,
-            "origin": origin,
-        },
+        headers=headers,
     ).json()
 
 
@@ -99,12 +82,7 @@ def get_slave_list(id):
     """Возвращает список рабов пользователя."""
     return get(
         f"https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/slaveList?id={id}",
-        headers={
-            "Content-Type": "application/json",
-            "authorization": auth,
-            "User-agent": user_agent,
-            "origin": origin,
-        },
+        headers=headers,
     ).json()
 
 
@@ -112,12 +90,7 @@ def get_top_users():
     """Возвращает список топ игроков."""
     return get(
         "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/topUsers",
-        headers={
-            "Content-Type": "application/json",
-            "authorization": auth,
-            "User-agent": user_agent,
-            "origin": origin,
-        },
+        headers=headers,
     ).json()
 
 
@@ -125,12 +98,7 @@ def get_start():
     """Получает полную информацию о своём профиле."""
     return get(
         "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/start",
-        headers={
-            "Content-Type": "application/json",
-            "authorization": auth,
-            "User-agent": user_agent,
-            "origin": origin,
-        },
+        headers=headers,
     ).json()
 
 
@@ -160,7 +128,7 @@ def upgrade_slave(slave_id):
                 pass
 
 
-def upgrade_slaves():
+def slaves_upgrade():
     """Прокачивает рабов, чтобы они приносили 1000 в минуту."""
     while True:
         try:
@@ -218,16 +186,16 @@ def buy_top_users_slaves():
                                         profile = get_buy_slave(slave_id)
 
                                         print(
-                                            f"""\n==[{strftime("%d.%m.%Y %H:%M:%S")}]==
+                                            f"""[{strftime("%H:%M:%S")}]
 Купил id{slave_info["id"]} за {slave_info["price"]} у id{top_user["id"]}
 Баланс: {"{:,}".format(profile['balance'])}
 Рабов: {"{:,}".format(profile['slaves_count'])}
 Доход в минуту: {"{:,}".format(profile['slaves_profit_per_min'])}
-Место в топе дохода: {"{:,}".format(profile['rating_position'])}\n""",
+Место в топе дохода: {"{:,}".format(profile['rating_position'])}""",
                                         )
 
                                         # Прокачивает раба
-                                        if conf_upgrade_slaves == 1:
+                                        if upgrade_slaves == 1:
                                             upgrade_slave(slave_id)
 
                                         # Покупает оковы только что купленному рабу
@@ -247,7 +215,7 @@ def buy_top_users_slaves():
 
 
 def buy_slaves():
-    """Покупает и улучшает рабов, надевает оковы, если включено в config.json."""
+    """Покупает и улучшает рабов, надевает оковы, если включено в config.py."""
     while True:
         try:
             # Случайный раб в промежутке
@@ -271,16 +239,16 @@ def buy_slaves():
 
             if "balance" in profile.keys():
                 print(
-                    f"""\n==[{strftime("%d.%m.%Y %H:%M:%S")}]==
+                    f"""[{strftime("%H:%M:%S")}]
 Купил id{slave_info["id"]} за {slave_info["price"]}
 Баланс: {"{:,}".format(profile['balance'])}
 Рабов: {"{:,}".format(profile['slaves_count'])}
 Доход в минуту: {"{:,}".format(profile['slaves_profit_per_min'])}
-Место в топе дохода: {"{:,}".format(profile['rating_position'])}\n""",
+Место в топе дохода: {"{:,}".format(profile['rating_position'])}""",
                 )
 
                 # Прокачивает раба
-                if conf_upgrade_slaves == 1:
+                if upgrade_slaves == 1:
                     upgrade_slave(slave_id)
 
                 # Покупает оковы только что купленному рабу
@@ -294,11 +262,11 @@ def buy_slaves():
             sleep(uniform(min_delay, max_delay))
 
 
-def buy_from_ids():
-    """То же самое, что и buy_slaves, только перекупает рабов из списка в config.json."""
+def buy_slaves_from_ids():
+    """То же самое, что и buy_slaves, только перекупает рабов из списка в config.py."""
     while True:
         try:
-            for id in buy_from_ids_list:
+            for id in buy_from_ids:
                 slaves = get_slave_list(id)
                 if "slaves" in slaves.keys():
                     for slave in slaves["slaves"]:
@@ -321,16 +289,16 @@ def buy_from_ids():
                                     profile = get_buy_slave(slave_id)
 
                                     print(
-                                        f"""\n==[{strftime("%d.%m.%Y %H:%M:%S")}]==
+                                        f"""[{strftime("%H:%M:%S")}]
 Купил id{slave_info["id"]} за {slave_info["price"]} у id{id}
 Баланс: {"{:,}".format(profile['balance'])}
 Рабов: {"{:,}".format(profile['slaves_count'])}
 Доход в минуту: {"{:,}".format(profile['slaves_profit_per_min'])}
-Место в топе дохода: {"{:,}".format(profile['rating_position'])}\n""",
+Место в топе дохода: {"{:,}".format(profile['rating_position'])}""",
                                     )
 
                                     # Прокачивает раба
-                                    if conf_upgrade_slaves == 1:
+                                    if upgrade_slaves == 1:
                                         upgrade_slave(slave_id)
 
                                     # Покупает оковы только что купленному рабу
@@ -346,7 +314,7 @@ def buy_from_ids():
             sleep(uniform(min_delay, max_delay))
 
 
-def buy_fetters():
+def set_fetters():
     """Покупает оковы тем, у кого их нет."""
     while True:
         try:
@@ -355,7 +323,7 @@ def buy_fetters():
                 slaves = start["slaves"]
                 # Удаление первого раба из списка,
                 # чтобы не происходило коллизии с прокачкой
-                if conf_upgrade_slaves == 1:
+                if upgrade_slaves == 1:
                     del slaves[0]
 
                 # Перебор списка рабов
@@ -380,7 +348,7 @@ def job_slaves():
             start = get_start()
             if "slaves" in start.keys():
                 slaves = start["slaves"]
-                if buy_slaves_mode == 0 and conf_buy_fetters == 1:
+                if buy_slaves_mode == 0 and buy_fetters == 1:
                     del slaves[0]
                 # Перебор списка рабов
                 for slave in slaves:
@@ -400,41 +368,24 @@ if __name__ == "__main__":
 github.com/monosans/vk-slaves-bot
 Версия 4.0""",
     )
-
-    # Конфиг
-    with open("config.json") as f:
-        try:
-            config = load(f)
-        except:
-            print("Конфиг настроен некорректно.")
-            sys.exit()
-    auth = str((config["authorization"]).strip())
-    buy_slaves_mode = int(config["buy_slaves_mode"])
-    conf_buy_fetters = int(config["buy_fetters"])
-    max_fetter_price = int(config["max_fetter_price"])
-    min_delay = int(config["min_delay"])
-    max_delay = int(config["max_delay"])
-    job = list(config["job"])
-    min_price = int(config["min_price"])
-    max_price = int(config["max_price"])
-    my_id = int(config["my_id"])
-    conf_upgrade_slaves = int(config["upgrade_slaves"])
-    buy_from_ids_list = list(config["buy_from_ids"])
-    user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.105 Safari/537.36"
-    origin = "https://prod-app7794757-c1ffb3285f12.pages-ac.vk-apps.com"
-
+    headers = {
+        "Content-Type": "application/json",
+        "authorization": authorization,
+        "User-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.51 Safari/537.36",
+        "origin": "https://prod-app7794757-c1ffb3285f12.pages-ac.vk-apps.com",
+    }
     # Запуск
     if buy_slaves_mode == 1:
-        print("Включена покупка случайных рабов.")
+        print("Включена покупка случайных рабов")
         Thread(target=buy_slaves).start()
     elif buy_slaves_mode == 2:
-        print("Включена перекупка рабов у топеров.")
+        print("Включена перекупка рабов у топеров")
         Thread(target=buy_top_users_slaves).start()
     elif buy_slaves_mode == 3:
-        print("Включена перекупка у IDшников из config.json.")
-        Thread(target=buy_from_ids).start()
-    if conf_upgrade_slaves == 1 and buy_slaves_mode == 0:
+        print("Включена перекупка у IDшников из config.py")
+        Thread(target=buy_slaves_from_ids).start()
+    if upgrade_slaves == 1 and buy_slaves_mode == 0:
         Thread(target=upgrade_slaves).start()
-    if conf_buy_fetters == 1:
+    if buy_fetters == 1:
         Thread(target=buy_fetters).start()
     Thread(target=job_slaves).start()
---
+++ config.py
@@ -0,0 +1,12 @@
+authorization = ""
+my_id = 607137534
+buy_slaves_mode = 1
+buy_from_ids = [380681805, 596833590, 328372664]
+buy_fetters = 1
+upgrade_slaves = 0
+min_price = 40
+max_price = 26151
+max_fetter_price = 35303
+jobs = ["github.com/monosans"]
+min_delay = 2.5
+max_delay = 4.5
